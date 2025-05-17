87cdb6e15cfa50aa4bb0104940ea9c1735e0860a
DarkCommit <gabrielraialsantos@gmail.com>
2025-05-17 09:15:57 -0300

    add handle module and handles basic initialize

--- handler/DeleteOpeningHandler.go
+++ handler/DeleteOpeningHandler.go
@@ -1,1 +1,11 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func DeleteOpeningHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"msg": "DELETE Opening"})
+}
--- handler/UpdateOpeningHandler.go
+++ handler/UpdateOpeningHandler.go
@@ -1,1 +1,11 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UpdateOpeningHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"msg": "PUT Opening"})
+}

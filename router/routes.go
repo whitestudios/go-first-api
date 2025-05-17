1064150efcc827ec856ebafc190579b9ba0b84c8
DarkCommit <gabrielraialsantos@gmail.com>
2025-05-17 00:43:01 -0300

    add routes logic

---
+++ router/routes.go
@@ -0,0 +1,33 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initializeRoutes(router *gin.Engine) {
+	v1 := router.Group("api/v1")
+	{
+		v1.GET("/opening", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"msg": "GET Opening"})
+		})
+
+		v1.POST("/opening", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"msg": "POST Opening"})
+		})
+
+		v1.DELETE("/opening", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"msg": "DELETE Opening"})
+		})
+
+		v1.PUT("/opening", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"msg": "PUT Opening"})
+		})
+
+		v1.GET("/openings", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"msg": "GET Openings"})
+		})
+
+	}
+}

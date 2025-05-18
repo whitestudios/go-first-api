28dc6dbca24e45bd50a766366a8eb5316c860ec6
DarkCommit <gabrielraialsantos@gmail.com>
2025-05-18 01:55:45 -0300

    add show opening handler and UpdateOpeningRequest Validate and struct

--- handler/ShowOpeningHandler.go
+++ handler/ShowOpeningHandler.go
@@ -4,8 +4,22 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/whitestudios/go-first-api/schemas"
 )
 
 func ShowOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"msg": "GET Opening"})
+	opening := schemas.Opening{}
+	id := c.Query("id")
+
+	if id == "" {
+		sendError(c, http.StatusBadRequest, errParamIsValid("id", "query-parameter").Error())
+		return
+	}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(c, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	sendSuccess(c, "show-opening", opening)
 }
--- handler/request.go
+++ handler/request.go
@@ -34,3 +34,21 @@ func (r *CreateOpeningRequest) Validate() error {
 	}
 	return nil
 }
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (o *UpdateOpeningRequest) Validate() error {
+	// if any field is provided, validation is truty
+	if o.Role != "" || o.Company != "" || o.Link != "" || o.Location != "" || o.Remote != nil || o.Salary > 0 {
+		return nil
+	}
+	// if none of the fields are provided, returns error
+	return fmt.Errorf("at least one field on request must be provided")
+}

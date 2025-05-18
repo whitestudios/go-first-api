ff0fc25354b71890d28bae1ff85f5a46775e3025
DarkCommit <gabrielraialsantos@gmail.com>
2025-05-18 19:22:52 -0300

    add documentation

--- router/routes.go
+++ router/routes.go
@@ -2,13 +2,21 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	docs "github.com/whitestudios/go-first-api/docs"
 	"github.com/whitestudios/go-first-api/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Initialize handler
+	BasePath := "/api/v1"
+
 	handler.InitializeHandler()
-	v1 := router.Group("api/v1")
+
+	docs.SwaggerInfo.BasePath = BasePath
+
+	v1 := router.Group(BasePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
@@ -17,4 +25,6 @@ func initializeRoutes(router *gin.Engine) {
 		v1.GET("/openings", handler.ListOpeningsHandler)
 
 	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }

979761cf76f16b86307fc04a621b8ef055ded3b2
DarkCommit <gabrielraialsantos@gmail.com>
2025-05-18 01:55:09 -0300

    add list opening handler

--- handler/ListOpeningsHandler.go
+++ handler/ListOpeningsHandler.go
@@ -4,8 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/whitestudios/go-first-api/schemas"
 )
 
 func ListOpeningsHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"msg": "GET Openigns"})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(c, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(c, "list-openings", openings)
 }

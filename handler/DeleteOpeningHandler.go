4d233305b7aa21c04ab78411c107b12695ae07a9
DarkCommit <gabrielraialsantos@gmail.com>
2025-05-18 01:54:53 -0300

    add delete opening

--- handler/DeleteOpeningHandler.go
+++ handler/DeleteOpeningHandler.go
@@ -1,11 +1,35 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/whitestudios/go-first-api/schemas"
 )
 
 func DeleteOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"msg": "DELETE Opening"})
+	id := c.Query("id")
+
+	if id == "" {
+		sendError(c, http.StatusBadRequest, errParamIsValid("id", "query-parameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+	// On Gorm, we first find the entity (opening) and then, we delete
+
+	// Find Opening
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(c, http.StatusNotFound, fmt.Sprintf("opening with id: %v not found", id))
+		return
+	}
+
+	// Delete Opening
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(c, http.StatusInternalServerError, fmt.Sprintf("error deleting opening with id: %v", id))
+		return
+	}
+
+	sendSuccess(c, "delete-opening", opening)
 }
